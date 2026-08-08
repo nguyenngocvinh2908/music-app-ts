73263a5129f3ec417bfa2481ad87602c265f59bf
Vinh Ga <nguyenngocvinh770@gmai.com>
2026-08-08 09:58:33 +0700

    [#3] Configraton Pug

--- index.ts
+++ index.ts
@@ -2,8 +2,12 @@ import express, { Request, Response, Application } from 'express'
 const app: Application = express()
 const port: number = 3000
 
+// Pug
+app.set('views', './views') // Specify the views directory
+app.set('view engine', 'pug')
+
 app.get('/topics', (req: Request, res: Response) => {
-  res.send('List of topics')
+  res.render('client/pages/topics/index', { title: 'Topics Page' })
 })
 
 app.listen(port, () => {
