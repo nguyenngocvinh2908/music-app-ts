da810f62e695ff464c293d45bc190cd824e10beb
Vinh Ga <nguyenngocvinh770@gmai.com>
2026-08-08 10:25:02 +0700

    [#4] Connect Database

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response, Application } from 'express'
+import dotenv from 'dotenv'
+import * as database from './config/database'
 const app: Application = express()
-const port: number = 3000
+const port: number | string = process.env.PORT || 3000
 
 // Pug
 app.set('views', './views') // Specify the views directory
 app.set('view engine', 'pug')
+
+// Env
+dotenv.config()
+
+// Database
+database.connectDatabase()
 
 app.get('/topics', (req: Request, res: Response) => {
   res.render('client/pages/topics/index', { title: 'Topics Page' })
