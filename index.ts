7dfc52f79dd32241e9a0743593a296ef6aaa1e68
Vinh Ga <nguyenngocvinh770@gmai.com>
2026-08-08 09:43:27 +0700

    [#2] Router Base

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("OK")
+import express, { Request, Response, Application } from 'express'
+const app: Application = express()
+const port: number = 3000
+
+app.get('/topics', (req: Request, res: Response) => {
+  res.send('List of topics')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
