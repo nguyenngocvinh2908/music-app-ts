b453605d54cbd46e8f6a417ffd17b96476875400
Vinh Ga <nguyenngocvinh770@gmai.com>
2026-08-17 23:32:25 +0700

    [#21] Finish Project

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ import methodOverride from 'method-override';
 import * as database from './config/database'
 import clientRoutes from './routes/client'
 import adminRoutes from './routes/admin'
-import { systemConfig } from 'config/config'
+import { systemConfig } from './config/config'
 import path from 'path'
 
 const app: Express = express()
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Pug
-app.set('views', './views') // Specify the views directory
+app.set('views', path.join(process.cwd(), 'views')) // Specify the views directory
 app.set('view engine', 'pug')
 
 
@@ -32,7 +32,7 @@ app.locals.prefixAdmin = systemConfig.prefixAdmin
 app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
 
 // Nhúng file tĩnh (CSS, JS, hình ảnh,...) vào ứng dụng
-app.use(express.static('public'))
+app.use(express.static(path.join(process.cwd(), 'public')))
 
 // Database
 database.connectDatabase()
@@ -41,7 +41,8 @@ database.connectDatabase()
 adminRoutes(app)
 clientRoutes(app)
 
-
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
+
+export default app
